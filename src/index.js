cc9d652bf603105c49b47e4bf2cd2fd1d7506ff4
MrKoga <evan.koga@gmail.com>
2020-10-30 18:24:36 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+ReactDOM.render(
+  <div>
+    <h1>this the title</h1>
+    <p> this si the body </p>
+  </div>,
+  document.getElementById("root")
+);
